60157fc884b3a0a2d0e03e18cabd518371e4cf03
jhs <jhs@nicc.kr>
2025-01-08 14:21:54 +0900

    Feat: 앱바 중복된 코드 함수로 작성 및 텍스트와 아이콘 배치 수정(3)

--- app/src/main/java/com/example/overvieworganization/MainActivity.kt
+++ app/src/main/java/com/example/overvieworganization/MainActivity.kt
@@ -5,9 +5,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -34,6 +39,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -74,10 +80,8 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun MainScreen() {
     val navController = rememberNavController()
-
     Scaffold(
         modifier = Modifier.fillMaxSize(),
-
         topBar = {
             TopBar {
                 navController.navigate(it) {
@@ -88,7 +92,6 @@ fun MainScreen() {
                 }
             }
         },
-
         bottomBar = {
             BottomNavigationBar {
                 navController.navigate(it) {
@@ -106,34 +109,30 @@ fun MainScreen() {
             modifier = Modifier.padding(innerPadding),
         ) {
             composable(route = HomeScreen.Home.name) {
-                HomeList{  navController.navigate(it) {
-                    launchSingleTop = true
-                    popUpTo(it) {
-                        inclusive = true
+                HomeList {
+                    navController.navigate(it) {
+                        launchSingleTop = true
+                        popUpTo(it) {
+                            inclusive = true
+                        }
                     }
-                }}
+                }
             }
-
             composable(route = EmployeeScreen.Employee.name) {
                 EmployeeList()
             }
-
             composable(route = LocateScreen.Locate.name) {
                 LocateList()
             }
-
             composable(route = ImageScreen.Image.name) {
                 ImageList()
             }
-
             composable(route = PromoteScreen.Promote.name) {
                 PromoteList()
             }
-
             composable(route = InfoScreen.Info.name) {
                 InfoList()
             }
-
             composable(route = TourScreen.Tour.name) {
                 TourList()
             }
@@ -147,214 +146,88 @@ fun MainScreen() {
 fun TopBar(onNavigate: (String) -> Unit) {
     val viewModel: AppViewModel = AppViewModel.getInstance()
     val data by viewModel.data.observeAsState("처음화면")
-
-
     TopAppBar(
+        modifier = Modifier.fillMaxWidth().fillMaxHeight(0.09F),
         title = {
             when (data) {
                 "처음화면" -> {
-                    Row(
-                        modifier = Modifier
-                            .padding(0.dp)
-                            .offset(x = 15.dp),//텍스트와 아이콘 모두 오른쪽으로 이동
-                        horizontalArrangement = Arrangement.spacedBy(20.dp)// 아이콘만 오른쪽으로 이동
-                    ){
-                        Text("처음화면", fontSize = 40.sp, fontWeight = FontWeight.Bold)
-
-                        IconButton(
-                            onClick = {
-                                onNavigate(HomeScreen.Home.name)
-                            }
-                        ) {
-                            Icon(
-                                imageVector = Icons.Default.Home,
-                                contentDescription = "시작 아이콘",
-                                tint = MaterialTheme.colorScheme.secondary,
-                                modifier = Modifier
-                                    .size(50.dp)
-                                    .offset(y = 5.dp)
-                            )
-                        }
-                    }
+                    TopTitleView("처음화면",onNavigate)
                 }
                 "직원안내" -> {
-                    Row(
-                        modifier = Modifier
-                            .padding(0.dp)
-                            .offset(x = 15.dp),
-                        horizontalArrangement = Arrangement.spacedBy(20.dp)
-                    ){
-                        Text("직원안내", fontSize = 40.sp, fontWeight = FontWeight.Bold)
-
-                        IconButton(
-                            onClick = {
-                                onNavigate(HomeScreen.Home.name)
-                            }
-                        ) {
-                            Icon(
-                                imageVector = Icons.Default.Home,
-                                contentDescription = "시작 아이콘",
-                                tint = MaterialTheme.colorScheme.secondary,
-                                modifier = Modifier
-                                    .size(50.dp)
-                                    .offset(y = 5.dp)
-                            )
-                        }
-                    }
+                    TopTitleView("직원안내",onNavigate)
                 }
                 "청사안내" -> {
-                    Row(
-                        modifier = Modifier
-                            .padding(0.dp)
-                            .offset(x = 15.dp),
-                        horizontalArrangement = Arrangement.spacedBy(20.dp)
-                    ){
-                        Text("청사안내", fontSize = 40.sp, fontWeight = FontWeight.Bold)
-
-                        IconButton(
-                            onClick = {
-                                onNavigate(HomeScreen.Home.name)
-                            }
-                        ) {
-                            Icon(
-                                imageVector = Icons.Default.Home,
-                                contentDescription = "시작 아이콘",
-                                tint = MaterialTheme.colorScheme.secondary,
-                                modifier = Modifier
-                                    .size(50.dp)
-                                    .offset(y = 5.dp)
-                            )
-                        }
-                    }
+                    TopTitleView("청사안내",onNavigate)
                 }
                 "포토갤러리" -> {
-                    Row(
-                        modifier = Modifier
-                            .padding(0.dp)
-                            .offset(x = 15.dp),
-                        horizontalArrangement = Arrangement.spacedBy(20.dp)
-                    ){
-                        Text("포토갤러리", fontSize = 40.sp, fontWeight = FontWeight.Bold)
-
-                        IconButton(
-                            onClick = {
-                                onNavigate(HomeScreen.Home.name)
-                            }
-                        ) {
-                            Icon(
-                                imageVector = Icons.Default.Home,
-                                contentDescription = "시작 아이콘",
-                                tint = MaterialTheme.colorScheme.secondary,
-                                modifier = Modifier
-                                    .size(50.dp)
-                                    .offset(y = 5.dp)
-                            )
-                        }
-                    }
+                    TopTitleView("포토갤러리",onNavigate)
                 }
                 "홍보컨텐츠" -> {
-                    Row(
-                        modifier = Modifier
-                            .padding(0.dp)
-                            .offset(x = 15.dp),
-                        horizontalArrangement = Arrangement.spacedBy(20.dp)
-                    ){
-                        Text("홍보컨텐츠", fontSize = 40.sp, fontWeight = FontWeight.Bold)
-
-                        IconButton(
-                            onClick = {
-                                onNavigate(HomeScreen.Home.name)
-                            }
-                        ) {
-                            Icon(
-                                imageVector = Icons.Default.Home,
-                                contentDescription = "시작 아이콘",
-                                tint = MaterialTheme.colorScheme.secondary,
-                                modifier = Modifier
-                                    .size(50.dp)
-                                    .offset(y = 5.dp)
-                            )
-                        }
-                    }
+                    TopTitleView("홍보컨텐츠",onNavigate)
                 }
                 "공지사항" -> {
-                    Row(
-                        modifier = Modifier
-                            .padding(0.dp)
-                            .offset(x = 15.dp),
-                        horizontalArrangement = Arrangement.spacedBy(20.dp)
-                    ){
-                        Text("공지사항", fontSize = 40.sp, fontWeight = FontWeight.Bold)
-
-                        IconButton(
-                            onClick = {
-                                onNavigate(HomeScreen.Home.name)
-                            }
-                        ) {
-                            Icon(
-                                imageVector = Icons.Default.Home,
-                                contentDescription = "시작 아이콘",
-                                tint = MaterialTheme.colorScheme.secondary,
-                                modifier = Modifier
-                                    .size(50.dp)
-                                    .offset(y = 5.dp)
-                            )
-                        }
-                    }
+                    TopTitleView("공지사항",onNavigate)
                 }
                 "관광안내" -> {
-                    Row(
-                        modifier = Modifier
-                            .padding(0.dp)
-                            .offset(x = 15.dp),
-                        horizontalArrangement = Arrangement.spacedBy(20.dp)
-                    ){
-                        Text("관광안내", fontSize = 40.sp, fontWeight = FontWeight.Bold)
-
-                        IconButton(
-                            onClick = {
-                                onNavigate(HomeScreen.Home.name)
-                            }
-                        ) {
-                            Icon(
-                                imageVector = Icons.Default.Home,
-                                contentDescription = "시작 아이콘",
-                                tint = MaterialTheme.colorScheme.secondary,
-                                modifier = Modifier
-                                    .size(50.dp)
-                                    .offset(y = 5.dp)
-                            )
-                        }
-                    }
+                    TopTitleView("관광안내",onNavigate)
                 }
             }
         },
         navigationIcon = {
-            IconButton(
-                onClick = {
+            Row(
+                modifier = Modifier.offset(y = 25.dp)
+            ){
+                IconButton(
+                    onClick = {}
+                ) {
+                    Icon(
+                        imageVector = Icons.Default.Star,
+                        contentDescription = "시작 아이콘",
+                        tint = MaterialTheme.colorScheme.secondary,
+                        modifier = Modifier.size(40.dp)
+                    )
                 }
-            ) {
-                Icon(
-                    imageVector = Icons.Default.Star,
-                    contentDescription = "시작 아이콘",
-                    tint = MaterialTheme.colorScheme.secondary,
-                    modifier = Modifier.size(40.dp)
-                )
             }
         },
         actions = {
             val calendar: Calendar = Calendar.getInstance()
             val sdf = SimpleDateFormat("yyyy.MM.dd.(E) HH:mm a")
-
             val result = sdf.format(calendar.time)
-
-            Text(result, fontSize = 30.sp, fontWeight = FontWeight.Bold, color = MaterialTheme.colorScheme.tertiary)
+            Row(
+                modifier = Modifier.offset(y = 25.dp)
+            ){
+                Text(result, fontSize = 32.sp, fontWeight = FontWeight.Bold, color = MaterialTheme.colorScheme.tertiary)
+            }
         },
         colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
             containerColor = MaterialTheme.colorScheme.primary,
             titleContentColor = MaterialTheme.colorScheme.secondary,
         ),
     )
+}
+
+@Composable
+fun TopTitleView(text: String,onNavigate: (String) -> Unit) {
+    Row(
+        modifier = Modifier.offset(x = 15.dp, y = 20.dp),//텍스트와 아이콘 모두 오른쪽으로 이동
+        horizontalArrangement = Arrangement.spacedBy(20.dp)// 아이콘만 오른쪽으로 이동
+    ) {
+        Text(text, fontSize = 40.sp, fontWeight = FontWeight.Bold)
+        IconButton(
+            onClick = {
+                onNavigate(HomeScreen.Home.name)
+            }
+        ) {
+            Icon(
+                imageVector = Icons.Default.Home,
+                contentDescription = "시작 아이콘",
+                tint = MaterialTheme.colorScheme.secondary,
+                modifier = Modifier
+                    .size(50.dp)
+                    .offset(y = 5.dp)
+            )
+        }
+    }
 }
 
 @Composable
@@ -493,4 +366,5 @@ fun BottomNavigationBar(onNavigate: (String) -> Unit) {
             }
         )
     }
+
 }
