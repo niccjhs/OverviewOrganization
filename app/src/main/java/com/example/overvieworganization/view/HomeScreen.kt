b2e4eb2eec8a7e68b5be21f24f33ebef5f3aafdc
jhs <jhs@nicc.kr>
2025-01-08 18:04:44 +0900

    Feat: 처음화면 관광안내/직원안내 사각형으로 구분

--- app/src/main/java/com/example/overvieworganization/view/HomeScreen.kt
+++ app/src/main/java/com/example/overvieworganization/view/HomeScreen.kt
@@ -4,12 +4,19 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
@@ -25,132 +32,94 @@ fun HomeList(onNavigate: (String) -> Unit) {
     val viewModel: AppViewModel = AppViewModel.getInstance()
     viewModel.setData("처음화면")
     Row(
-        modifier = Modifier.padding(250.dp).background(color = MaterialTheme.colorScheme.background).background(color = Color.DarkGray),
+        modifier = Modifier.padding(250.dp).background(color = MaterialTheme.colorScheme.background),
         horizontalArrangement = Arrangement.spacedBy(400.dp),
         //text와 카드로 구분
     ) {
         Column(
-            modifier = Modifier.padding(0.dp),
             verticalArrangement = Arrangement.spacedBy(20.dp)
             //text 안에 있는 text들 구분
         ) {
             Column(
-                modifier = Modifier.padding(0.dp),
-                verticalArrangement = Arrangement.spacedBy(0.dp)
                 //text에 한글 글자 폰트 구분
             ){
-                Text(
-                    text = "한눈에",
-                    fontSize = 40.sp,
-                    fontWeight = FontWeight.Bold,
-                )
-                Text(
-                    text = "새로운 세상을",
-                    fontSize = 40.sp,
-                    fontWeight = FontWeight.Bold,
-                )
-                Text(
-                    text = "만나다.",
-                    fontSize = 40.sp,
-                    fontWeight = FontWeight.Bold,
-                )
+                Text(text = "한눈에", fontSize = 40.sp, fontWeight = FontWeight.Bold, color = Color.White)
+                Text(text = "새로운 세상을", fontSize = 40.sp, fontWeight = FontWeight.Bold, color = Color.White)
+                Text(text = "만나다.", fontSize = 40.sp, fontWeight = FontWeight.Bold, color = Color.White)
             }
             Row(
-                modifier = Modifier.padding(0.dp),
-                horizontalArrangement = Arrangement.spacedBy(0.dp)
                 //text에 영어 글자 폰트 구분
             ){
-                Text(
-                    text = "Meet a new World ",
-                    fontSize = 30.sp,
-                    fontWeight = FontWeight.Normal,
-                )
-                Text(
-                    text = "at a glance.",
-                    fontSize = 30.sp,
-                    fontWeight = FontWeight.Bold,
-                )
+                Text(text = "Meet a new World ", fontSize = 30.sp, fontWeight = FontWeight.Normal, color = Color.White)
+                Text(text = "at a glance.", fontSize = 30.sp, fontWeight = FontWeight.Bold, color = Color.White)
             }
         }
         Row(
-            modifier = Modifier.padding(0.dp),
+            modifier = Modifier.size(width = 300.dp, height = 1000.dp),
             horizontalArrangement = Arrangement.spacedBy(100.dp)
             //카드 안에 있는 카드1과 카드2로 구분
         ) {
             Column(
-                modifier = Modifier.padding(0.dp),
-                verticalArrangement = Arrangement.spacedBy(100.dp)
+                verticalArrangement = Arrangement.spacedBy(20.dp)
                 //카드1에 있는 2가지 버튼을 구분
             ){
                 TextButton(
                     onClick = {
                         onNavigate(TourScreen.Tour.name)
-                    }
+                    },
+                    modifier = Modifier.size(width = 300.dp, height = 200.dp).background(color = Color.White).offset(y = (-20).dp)
                 ){
                     Column(
-                        modifier = Modifier.padding(0.dp),
                         verticalArrangement = Arrangement.spacedBy(10.dp)
                         //카드1에 관광안내 버튼에서 제목과 소제목 구분
                     ){
-                        Text(
-                            text = "관광 안내",
-                            fontSize = 30.sp,
-                            fontWeight = FontWeight.Bold,
-                        )
+                        Text(text = "관광 안내", fontSize = 30.sp, fontWeight = FontWeight.ExtraBold)
                         Column(
-                            modifier = Modifier.padding(0.dp),
                             verticalArrangement = Arrangement.spacedBy(0.dp)
                             //카드1에 관광안내 버튼에서 소제목을 구분
                         ){
-                            Text(
-                                text = "민원실 좌석 배치도 및 직원 정보를",
-                                fontSize = 15.sp,
-                                fontWeight = FontWeight.Normal,
-                            )
-                            Text(
-                                text = "한눈에 알아볼 수 있도록 소개합니다.",
-                                fontSize = 15.sp,
-                                fontWeight = FontWeight.Normal,
-                            )
+                            Text(text = "민원실 좌석 배치도 및 직원 정보를", fontSize = 15.sp, fontWeight = FontWeight.Normal)
+                            Text(text = "한눈에 알아볼 수 있도록 소개합니다.", fontSize = 15.sp, fontWeight = FontWeight.Normal)
                         }
                     }
                 }
                 TextButton(
                     onClick = {
                         onNavigate(EmployeeScreen.Employee.name)
-                    }
+                    },
+                    modifier = Modifier.size(width = 300.dp, height = 700.dp).background(MaterialTheme.colorScheme.secondary)
                 ){
                     Column(
-                        modifier = Modifier.padding(0.dp),
                         verticalArrangement = Arrangement.spacedBy(10.dp)
                         //카드1에 직원안내 버튼에서 제목과 소제목 구분
                     ) {
                         Text(
                             text = "직원 안내",
                             fontSize = 30.sp,
-                            fontWeight = FontWeight.Bold,
+                            fontWeight = FontWeight.ExtraBold,
+                            color = Color.White
                         )
                         Column(
-                            modifier = Modifier.padding(0.dp),
                             verticalArrangement = Arrangement.spacedBy(0.dp)
                             //카드1에 직원안내 버튼에서 소제목을 구분
                         ){
                             Text(
                                 text = "좌석배치도 및 담당자의 주요 업무를",
                                 fontSize = 15.sp,
-                                fontWeight = FontWeight.Normal,
+                                fontWeight = FontWeight.Bold,
+                                color = Color.White
                             )
                             Text(
                                 text = "소개합니다.",
                                 fontSize = 15.sp,
-                                fontWeight = FontWeight.Normal,
+                                fontWeight = FontWeight.Bold,
+                                color = Color.White
                             )
                         }
                     }
                 }
             }
             Column(
-                modifier = Modifier.padding(0.dp),
                 verticalArrangement = Arrangement.spacedBy(30.dp)
                 //카드2에 있는 3가지 버튼을 구분
             ) {
@@ -183,7 +152,6 @@ fun HomeList(onNavigate: (String) -> Unit) {
                     }
                 ){
                     Column(
-                        modifier = Modifier.padding(0.dp),
                         verticalArrangement = Arrangement.spacedBy(10.dp)
                         //카드2에 청사안내 버튼에서 제목과 소제목으로 구분
                     ){
@@ -205,7 +173,6 @@ fun HomeList(onNavigate: (String) -> Unit) {
                     }
                 ){
                     Column(
-                        modifier = Modifier.padding(0.dp),
                         verticalArrangement = Arrangement.spacedBy(10.dp)
                         //카드2에 공지사항 버튼에서 제목과 소제목으로 구분
                     ){
